1c70dcdb2f18eaecced521d41c2c2686c30984bb
luisflores-coded <64532712+luisflores-coded@users.noreply.github.com>
2020-04-29 15:23:18 +0300

    Check lifetime writes
    Endurance Rating (Lifetime Writes) indicates for how long a NVME will sustain it's performance and latency. This improved check uses the percentage_used value to issue WARNING above 80% and CRITICAL above 90%.
    Invokes nvme smart-log only once and cache result for all tests.

--- check_nvme.sh
+++ check_nvme.sh
@@ -5,6 +5,7 @@
 # https://github.com/sammcj/nagios/blob/master/check_nvme.sh
 # Maintainer : Pierre D. - https://dutiko.com/
 #
+# v2.4 : call nvme smart-log only once, verify lifetime writes - performance drops severely when percentage_used goes above 100%
 # v2.3 : check if script is runned as root/sudo, exit with unknown error if not
 # v2.2 : add check to detec if nvme disk is detected, exit with unknown error if not
 # v2.1 : add checks to detect if nvme-cli is present, exit with unknown error if not
@@ -21,37 +22,55 @@ if [ $(id -u) -ne 0 ] ; then echo "UNKNOWN: please run as root or with sudo" ; e
 
 DISKS=$(lsblk -e 11,253 -dn -o NAME | grep nvme)
 CRIT=false
+WARN=false
 MESSAGE=""
 
 command -v nvme >/dev/null 2>&1 || { echo >&2 "UNKNOWN: nvme-cli not found ; please install it" ; exit 3; }
 if [ -z "$DISKS" ] ; then echo "UNKNOWN: no nvme disks found"; exit 3; fi
 
 for DISK in $DISKS ; do
+  # capture disk SMART log
+  LOG=$(nvme smart-log /dev/$DISK)
+
   # Check for critical_warning
-  $(nvme smart-log /dev/$DISK | awk 'FNR == 2 && $3 != 0 {exit 1}')
+  $(echo "$LOG" | awk 'FNR == 2 && $3 != 0 {exit 1}')
   if [ $? == 1 ]; then
     CRIT=true
     MESSAGE="$MESSAGE $DISK has critical warning "
   fi
 
   # Check media_errors
-  $(nvme smart-log /dev/$DISK | awk 'FNR == 15 && $3 != 0 {exit 1}')
+  $(echo "$LOG" /dev/$DISK | awk 'FNR == 15 && $3 != 0 {exit 1}')
   if [ $? == 1 ]; then
     CRIT=true
     MESSAGE="$MESSAGE $DISK has media errors "
   fi
 
   # Check num_err_log_entries
-  $(nvme smart-log /dev/$DISK | awk 'FNR == 16 && $3 != 0 {exit 1}')
+  $(echo "$LOG" | awk 'FNR == 16 && $3 != 0 {exit 1}')
   if [ $? == 1 ]; then
     CRIT=true
     MESSAGE="$MESSAGE $DISK has errors logged "
+  fi
+
+  # Check percentage_used
+  $(echo "$LOG" | awk 'FNR == 6 {exit $3}')
+  PERC=$?
+  if [ $PERC -ge 90 ]; then
+    CRIT=true
+    MESSAGE="$MESSAGE $DISK has $PERC percentage used "
+  elif [ $PERC -ge 80 ]; then
+    WARN=true
+    MESSAGE="$MESSAGE $DISK has $PERC percentage used "
   fi
 done
 
 if [ $CRIT = "true" ]; then
   echo "CRITICAL: $MESSAGE"
   exit 2
+elif [ $WARN = "true" ]; then
+  echo "WARNING: $MESSAGE"
+  exit 1
 else
   echo "OK $(echo $DISKS | tr -d '\n')"
   exit 0
